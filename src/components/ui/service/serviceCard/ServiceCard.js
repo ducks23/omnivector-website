4a15a8627eedc7920870c7de640ad60cdca11c14
Cory Zimmerman <cory@coryzimmerman.com>
2020-02-24 12:03:00 -0800

    add card footer

--- src/components/ui/service/serviceCard/ServiceCard.js
+++ src/components/ui/service/serviceCard/ServiceCard.js
@@ -1,13 +1,55 @@
 import React, {Fragment} from 'react';
-import classes from './ServiceCard.module.css'
+// import classes from './ServiceCard.module.css'
 import { Helmet } from "react-helmet";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+  deploy: {
+    ...theme.typography.deploy,
+    borderRadius: "8px",
+    backgroundColor: theme.palette.primary.main,
+    margin: "0 30px 0 20px ",
+  },
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "white",
+    width: "30vw",
+    borderRadius: "16px",
+    margin: "0 3vw",
+    boxShadow: "4px 4px 8px rgba(0, 0, 0, .5)"
+  },
+  headerTitle: {
+    ...theme.typography.deploy,
+    display: "block",
+    textAlign: "left",
+    margin:  "16px 16px",
+    color: "black"
+  },
+  headerDescription: {
+    ...theme.typography.deploy,
+    display: "block",
+    textAlign: "left",
+    margin:  "16px 16px",
+    color: "black",
+    fontWeight: 200
+  },
+  footer: {
+    ...theme.typography.deploy,
+    display: "block",
+    margin:  "16px 16px",
+  }
+
+}));
 
 const ServiceCard = (props) => {
+  const classes = useStyles();
   return (
-    <div className={classes.Card}>
-      <div className={classes.CardHeader}>
-        <h4>{props.title}</h4>
-        <p>{props.description}</p>
+    <div className={classes.card}>
+      <div >
+        <h4 className={classes.headerTitle}>{props.title}</h4>
+        <p className={classes.headerDescription}>{props.description}</p>
       </div>
       <div>
         <Helmet>
@@ -19,6 +61,14 @@ const ServiceCard = (props) => {
           </script>
         </Helmet>
         <div class="juju-card" data-id={props.dataId} data-dd ></div>
+      </div>
+      <div className={classes.footer}>
+        <Button
+          className={classes.deploy}
+          variant="contained"
+        >
+          Get help with {props.title}...
+        </Button>
       </div>
     </div>
   )
--- src/components/ui/theme/Theme.js
+++ src/components/ui/theme/Theme.js
@@ -30,5 +30,23 @@ export default createMuiTheme({
       fontSize: "1 rem",
       color: "white",
     }
+  },
+  serviceCard: {
+    card: {
+      display: "flex",
+      flexDirection: "column",
+      backgroundColor: "white",
+      width: "30vw",
+      height: "725px",
+      borderRadius: "16px",
+      margin: "0 3vw",
+      boxShadow: "4px 4px 8px rgba(0, 0, 0, .5)"
+    },
+    header: {
+      display: "block",
+      textAlign: "left",
+      fontFamily: "Ubuntu",
+      margin:  "0px 16px"
+    }
   }
 })
